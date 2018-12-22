30797fd4e811438275fc41e28f0bd0446e039121
Davidood <dwhywood@gmail.com>
2018-12-21 23:42:21 -0500

    Retrieve a single record from resource

--- index.js
+++ index.js
@@ -19,4 +19,24 @@ app.get('/channel', (req, res) => {
   });
 });
 
+app.get('/channel/:id', (req, res) => {
+  fs.readFile(`${__dirname}/db/channel.json`, 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+      // eslint-disable-next-line no-console
+      console.log(err);
+    }
+
+    const channels = JSON.parse(data);
+
+    const item = channels.find(channel => parseInt(channel.id, 10) === parseInt(req.params.id, 10));
+
+    if (item) {
+      res.json(item);
+    } else {
+      res.status(404).json({ error: 'Channel doesn\'t exist' });
+    }
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
