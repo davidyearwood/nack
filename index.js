f472db78dffbf3d23995b943bc782b022e20368e
Davidood <dwhywood@gmail.com>
2018-12-23 22:23:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,4 +39,31 @@ app.get('/channel/:id', (req, res) => {
   });
 });
 
+app.post('/channel/:channelId/msg', (req, res) => {
+  if (!req.body) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+
+  let payload = {
+    sender: req.body.sender,
+    channelId: req.params.channelId,
+    msg: req.body.msg,
+    timestamp: req.body.timestamp,
+  };
+
+  fs.readFile(`${__dirname}/db/channel.json`, 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+      // eslint-disable-next-line no-console
+      console.log(err);
+    }
+
+    const channel = JSON.parse(data);
+    const found = channel.find(item => item.id === req.params.channelId);
+    payload.id = found.msg.length + 1;
+    found.msg.push(payload);
+    fs.writeFile(`${__dirname}/db/channel.json`, )
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
