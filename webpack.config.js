0cfe5f71226a67acf2b975924c57b80bae9ed4fb
Davidood <dwhywood@gmail.com>
2018-12-24 23:44:17 -0500

    Configure Web Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,26 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
       },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-        },
+    },
+    {
+      test: /\.html$/,
+      use: {
+        loader: 'html-loader',
       },
+    },
     ],
+  },
+  devServer: {
+    contentBase: './dist',
   },
   resolve: {
     alias: {
