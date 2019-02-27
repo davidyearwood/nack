a376ac438e8311d21ab5428bc8d5df053c61b045
Davidood <dwhywood@gmail.com>
2019-02-27 06:29:54 -0600

    Implement day divider details

--- src/components/presentational/Message/Messages.jsx
+++ src/components/presentational/Message/Messages.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { format } from "date-fns";
+import { format, isSameDay } from "date-fns";
 import Message from "./Message";
 import styles from "./message.css";
 
+const DayDivider = ({ date }) => (
+  <div className={styles.divider}>
+    <h3>{format(date, "dddd, MMMM Do")}</h3>
+  </div>
+);
 function Messages({ msgs }) {
   const msgContainer = React.createRef();
   return (
@@ -14,15 +19,24 @@ function Messages({ msgs }) {
         msgContainer.current.scrollTo(0, msgContainer.current.scrollHeight)
       }
     >
-      {msgs.map(msg => (
-        <Message
-          key={msg.id}
-          src="https://via.placeholder.com/75"
-          text={msg.msg}
-          time={format(msg.timestamp, "MMM D, YYYY HH:mm A")}
-          sender={msg.sender}
-          alt=""
-        />
+      {msgs.map((msg, index) => (
+        <React.Fragment>
+          {index > 0 &&
+          isSameDay(
+            new Date(msg.timestamp),
+            new Date(msgs[index - 1].timestamp)
+          ) ? null : (
+            <DayDivider date={msg.timestamp} />
+          )}
+          <Message
+            key={msg.id}
+            src="https://via.placeholder.com/75"
+            text={msg.msg}
+            time={format(msg.timestamp, "MMM D, YYYY HH:mm A")}
+            sender={msg.sender}
+            alt=""
+          />
+        </React.Fragment>
       ))}
     </div>
   );
