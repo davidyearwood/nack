340ab6215acb981bc466bd18802ea020f78d7944
Davidood <dwhywood@gmail.com>
2019-03-06 20:41:00 -0600

    Add controller to routes

--- backend/routes/api/channels.js
+++ backend/routes/api/channels.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const ChannelsController = require("../../controllers/channelsController");
 
 const router = express.Router();
 
-router.get("/channels");
-router.get("/channels/:id");
-router.get("/channels/:channelId/msg");
-router.get("/channels/:channelId/msg/:msgId");
-router.post("/channels");
-router.post("/channel/:channelId/msg");
+router.get("/channels", ChannelsController.channelList);
+
+router.get("/channels/:id", ChannelsController.channel);
+router.get("/channels/:channelId/msg", ChannelsController.channelMessages);
+router.get(
+  "/channels/:channelId/msg/:msgId",
+  ChannelsController.channelMessage
+);
+router.post("/channels", ChannelsController.addChannel);
+router.post("/channel/:channelId/msg", ChannelsController.addMessage);
 
 module.exports = router;
