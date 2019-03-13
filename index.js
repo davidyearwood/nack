ffd56fb83ee93d5b92dc83ce2385d0904be708b4
Davidood <dwhywood@gmail.com>
2019-03-13 16:17:47 -0500

    Upload file to Cloudinary

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const app = require("express")();
+const dotenv = require("dotenv").config();
+const express = require("express");
+
+const app = express();
 const server = require("http").Server(app);
 const bodyParser = require("body-parser");
 const io = require("socket.io")(server);
+const fs = require("fs");
+const fileType = require("file-type");
+const path = require("path");
+const cloudinary = require("cloudinary");
 const channels = require("./db/ChannelData");
 const channelApiRoutes = require("./backend/routes/api/channels");
 const ChannelsModel = require("./backend/models/channelsModel");
 const store = require("./backend/store");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const ChannelsModelInstance = new ChannelsModel(store);
+
+// configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+app.use(express.static("file_storage"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,6 +47,59 @@ io.on("connection", socket => {
     if (c) {
       socket.broadcast.emit("new channel", c);
     }
+  });
+
+  // evaluate the meta data - file name and path to make sure the file
+  // that is being stored is being stored properly in the currect directory
+  // and not a name that can overwrite a file
+
+  // File size and content (what is the file used for?)
+
+  socket.on("uploadfile", file => {
+    const filePath = path.parse(file.name);
+    const allowedMimeTypes = ["image/jpeg", "image/png"];
+
+    const { mime } = fileType(file.data);
+
+    if (allowedMimeTypes.includes(mime)) {
+      console.log("correct mime type");
+      const { name, ext } = path.parse(file.name);
+      const base64Img = `data:image/jpeg;base64,${Buffer.from(
+        file.data
+      ).toString("base64")}`;
+
+      cloudinary.v2.uploader.upload(base64Img, (err, result) => {
+        if (!err) {
+          console.log(result);
+        }
+
+        if (result) {
+          socket.emit("uploadFile", {
+            image: true,
+            type: file.type,
+            data: Buffer.from(file.data).toString("base64"),
+            src: "data:image/jpeg;base64,",
+            url: result.url
+          });
+        }
+      });
+    }
+    // if (whiteList[fileType(file.data).mime]) {
+    //   // perform some file validation
+    //   console.log("Valid");
+    //   fs.writeFile(`./file_storage/images/${filePath.base}`, file.data, err => {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+
+    //     socket.emit("uploadFile", {
+    //       image: true,
+    //       type: file.type,
+    //       data: Buffer.from(file.data).toString("base64"),
+    //       src: "data:image/jpeg;base64,"
+    //     });
+    //   });
+    // }
   });
 });
 
